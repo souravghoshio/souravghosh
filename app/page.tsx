c68722e1255b8a64d53bd91f26a2c96a4ad3c5af
souravghoshofficial <souravghosh.tech@gmail.com>
2026-07-09 12:26:38 +0530

    init: first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,116 @@
+import React from "react";
+import Link from "next/link";
 import Image from "next/image";
 
-export default function Home() {
+import { Moon } from "lucide-react";
+import { TextFlip } from "@/components/text-flip";
+import { ShimmeringText } from "@/components/shimmering-text"
+
+
+const WORDS = ["Software Developer.", "Problem Solver.", "Java Enthusiast."]
+
+const Home = () => {
+  const navLinks = [
+    {
+      text: "Home",
+      url: "/",
+    },
+    {
+      text: "About",
+      url: "/about",
+    },
+    {
+      text: "Projects",
+      url: "/projects",
+    },
+    {
+      text: "Skills",
+      url: "/skills",
+    },
+    {
+      text: "Contact",
+      url: "/contact",
+    },
+  ];
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="w-full min-h-screen">
+      <nav className="w-full fixed top-0 left-0 border-b border-white/10 bg-background">
+        <div className="flex w-full max-w-3xl items-center justify-between mx-auto px-4 py-3 border-l border-r border-white/10">
+          <div className="flex items-center gap-1.5">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src={"/profile-pic.png"}
+              alt="profile image"
+              width={24}
+              height={24}
+              className="rounded-full"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <span className="font-semibold">Sourav Ghosh</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <ul className="hidden md:flex items-center gap-4">
+              {navLinks.map((navlink) => (
+                <li key={navlink.url} className="text-sm">
+                  <Link
+                    className="font-medium text-gray-400 hover:text-gray-50"
+                    href={navlink.url}
+                  >
+                    {navlink.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <span className="text-xl font-light text-gray-500/50 pl-1">
+                |
+              </span>
+            </div>
+            <button className="hover:bg-zinc-800/50 p-2 rounded-md group">
+              <Moon size={16} className="group-hover:-rotate-12 " />
+            </button>
+          </div>
         </div>
-      </main>
-    </div>
+      </nav>
+      <div className="w-full max-w-3xl border-l border-r border-white/10 min-h-screen mx-auto">
+        <div>
+          <Image
+            src={"/bg-new.jpg"}
+            alt="backgroud image"
+            width={200}
+            height={200}
+            className="w-full h-56 md:h-80"
+          />
+        </div>
+        <div className="flex w-full border-b border-t border-white/10">
+          <div className="border-r border-white/10">
+            <Image
+              src={"/profile-pic.png"}
+              alt="profile picture"
+              width={100}
+              height={100}
+              className="h-auto w-32 md:w-48 aspect-square rounded-full"
+            />
+          </div>
+          <div className="mt-auto w-full">
+            <div className="w-full border-b border-t border-white/10 px-4">
+              <h3 className="text-3xl font-bold">Sourav Ghosh</h3>
+            </div>
+            <div className="px-4 py-1.5">
+              <TextFlip className="">
+                {WORDS.map((word) => (
+                  <ShimmeringText className="[--color:#4B5563] [--shimmering-color:#fff] font-mono text-balance" key={word} text={word}/>
+                ))}
+              </TextFlip>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
