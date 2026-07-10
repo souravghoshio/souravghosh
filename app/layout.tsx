f5530a4f04f9ca17c1c11fcc1adfdf51ffd34fa5
souravghoshofficial <souravghosh.tech@gmail.com>
2026-07-10 08:41:52 +0530

    add: hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${inter.variable} h-full antialiased`}
+      className={`${inter.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,48 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import { Moon } from "lucide-react";
+import { Moon, Mail, Phone, MapPin, Clock, Mars, Calendar } from "lucide-react";
+import {
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiTwitterXFill,
+} from "@remixicon/react";
 import { TextFlip } from "@/components/text-flip";
-import { ShimmeringText } from "@/components/shimmering-text"
+import { ShimmeringText } from "@/components/shimmering-text";
+
+import { Button } from "@/components/ui/button";
+
+import { CopyButton } from "@/components/copy-button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const WORDS = ["Software Developer.", "Problem Solver.", "Java Enthusiast."];
 
 
-const WORDS = ["Software Developer.", "Problem Solver.", "Java Enthusiast."]
+const timeFormatter = new Intl.DateTimeFormat("en-IN", {
+  timeZone: "Asia/Kolkata",
+  hour: "2-digit",
+  minute: "2-digit",
+  hour12: true,
+});
+
+const dayFormatter = new Intl.DateTimeFormat("en-IN", {
+  timeZone: "Asia/Kolkata",
+  weekday: "long",
+});
+
+const dateFormatter = new Intl.DateTimeFormat("en-IN", {
+  timeZone: "Asia/Kolkata",
+  day: "2-digit",
+  month: "short",
+  year: "numeric",
+});
 
 const Home = () => {
   const navLinks = [
@@ -32,6 +67,21 @@ const Home = () => {
       url: "/contact",
     },
   ];
+
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const time = timeFormatter.format(now);
+  const day = dayFormatter.format(now);
+  const date = dateFormatter.format(now);
+
 
   return (
     <main className="w-full min-h-screen">
@@ -79,7 +129,7 @@ const Home = () => {
             alt="backgroud image"
             width={200}
             height={200}
-            className="w-full h-56 md:h-80"
+            className="w-full h-56 md:h-72"
           />
         </div>
         <div className="flex w-full border-b border-t border-white/10">
@@ -97,16 +147,167 @@ const Home = () => {
               <h3 className="text-3xl font-bold">Sourav Ghosh</h3>
             </div>
             <div className="px-4 py-1.5">
-              <TextFlip className="">
+              <TextFlip interval={3}>
                 {WORDS.map((word) => (
-                  <ShimmeringText className="[--color:#4B5563] [--shimmering-color:#fff] font-mono text-balance" key={word} text={word}/>
+                  <ShimmeringText
+                    className="[--color:#4B5563] [--shimmering-color:#fff] font-mono text-sm text-balance"
+                    key={word}
+                    text={word}
+                  />
                 ))}
               </TextFlip>
             </div>
           </div>
         </div>
-        <div>
-          <p></p>
+        <div className="h-10 w-full pattern overflow-hidden" />
+        <div className="border-t border-b border-white/10 flex w-full">
+          <div className="w-1/2 border-r border-dashed border-white/10 p-3">
+            <div className="flex items-center gap-3 group">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <Mail className="w-4 text-gray-400" />
+              </div>
+              <Link
+                className="hover:underline underline-offset-3 font-mono text-sm tracking-tight"
+                href="mailto:sourav@souravcodes.com"
+                target="_blank"
+              >
+                sourav@souravcodes.com
+              </Link>
+              <CopyButton
+                className="opacity-0 group-hover:opacity-50 hover:opacity-100"
+                variant="ghost"
+                size="icon-sm"
+                text="sourav@souravcodes.com"
+              />
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <Phone className="w-4 text-gray-400" />
+              </div>
+              <a
+                className="hover:underline underline-offset-3 font-mono text-sm tracking-tight"
+                href="tel:+919734567676"
+              >
+                +91 97345 67676
+              </a>
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <MapPin className="w-4 text-gray-400" />
+              </div>
+              <a
+                className="hover:underline underline-offset-3 font-mono text-sm tracking-tight"
+                href="https://maps.app.goo.gl/PNDqHcRtFGFEgMrc8"
+              >
+                Kolkata, India
+              </a>
+            </div>
+          </div>
+          <div className="w-1/2 p-3">
+           <div className="flex items-center gap-3">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <Calendar className="w-4 text-gray-400" />
+              </div>
+              <span className="text-sm font-mono">{day}, {date}</span>
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <Clock className="w-4 text-gray-400" />
+              </div>
+              <span className="text-sm font-mono">{time.toUpperCase()}</span>
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <div className="h-6 w-6 flex items-center justify-center bg-zinc-800/80 border border-white/10 rounded-sm ring-1 ring-white/8 ring-offset-1 ring-offset-background">
+                <Mars className="w-4 text-gray-400" />
+              </div>
+              <span className="text-sm font-mono">he/him</span>
+            </div>
+          </div>
+        </div>
+        <div className="w-full border-b border-white/10 p-3">
+          <div className="flex items-center gap-2">
+            <Tooltip>
+              <TooltipTrigger
+                delay={0}
+                closeDelay={0}
+                render={
+                  <Link
+                    target="_blank"
+                    href="https://linkedin.com/in/souravghoshio"
+                  >
+                    <Button variant="outline" className="w-8 h-8 p-1">
+                      <RiLinkedinBoxFill size={36} />
+                    </Button>
+                  </Link>
+                }
+              />
+              <TooltipContent>
+                <p>LinkedIn (souravghoshio)</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger
+                delay={0}
+                closeDelay={0}
+                render={
+                  <Link target="_blank" href="https://x.com/souravghoshio">
+                    <Button variant="outline" className="w-8 h-8 p-1">
+                      <RiTwitterXFill />
+                    </Button>
+                  </Link>
+                }
+              />
+              <TooltipContent>
+                <p>X (@souravghoshio)</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger
+                delay={0}
+                closeDelay={0}
+                render={
+                  <Link target="_blank" href="https://github.com/souravghoshio">
+                    <Button variant="outline" className="w-8 h-8 p-1">
+                      <RiGithubFill />
+                    </Button>
+                  </Link>
+                }
+              />
+              <TooltipContent>
+                <p>GitHub (souravghoshio)</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+        <div className="h-10 w-full pattern overflow-hidden" />
+        <div className="border-t border-b border-white/10 p-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Repudiandae ex magnam tempore debitis, dolores, provident
+            praesentium dicta dignissimos odit ad rem minima veniam? Voluptatem
+            amet assumenda ducimus quo rerum dolorem repudiandae excepturi animi
+            in reprehenderit at, asperiores, eveniet nemo quos porro vitae
+            voluptate soluta, ad officiis. Amet dolor in nesciunt incidunt omnis
+            earum. Perferendis, provident fuga. Repellat ad tempora eius aut
+            minus delectus necessitatibus natus voluptates dicta. Perferendis
+            ipsam doloribus perspiciatis quis! Magni, excepturi eos. Accusamus
+            minima quis at excepturi eos ea ratione impedit fuga possimus sed,
+            doloribus cumque molestias magnam numquam fugit qui nam quisquam
+            quod. Asperiores inventore aspernatur animi vero at similique maxime
+            doloribus fugiat ipsa atque repellat quod corrupti officiis
+            voluptatum, voluptatem et sunt vel nobis culpa itaque ratione,
+            repudiandae dolor? Recusandae fugiat, laudantium distinctio aut vel
+            dignissimos voluptates repellat explicabo officia laboriosam.
+            Dolorum numquam excepturi commodi, facere at adipisci dolorem,
+            sapiente officiis amet eum vel fuga, nemo doloribus ex a ipsum
+            laudantium. At vel, asperiores rem id quae excepturi quas dicta
+            natus in mollitia accusantium quod veniam numquam ea libero
+            obcaecati dolores quam non, itaque odit provident enim velit tempora
+            dolorem? Nihil, reprehenderit soluta quos cumque enim tenetur saepe.
+            Unde vero laboriosam velit voluptatem deserunt quisquam!
+          </p>
         </div>
       </div>
     </main>
